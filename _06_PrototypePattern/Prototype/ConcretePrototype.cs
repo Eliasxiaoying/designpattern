970d656503082472bd3ea37b7d8d4cbf796fa369
Elias <ywh1537000505@gmail.com>
2022-10-04 10:25:04 +0800

    原型模式PrototypePattern

---
+++ _06_PrototypePattern/Prototype/ConcretePrototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_PrototypePattern.Prototype;
+
+public class ConcretePrototype : MonkeyKingPrototype
+{
+    public ConcretePrototype(string id) : base(id) { }
+
+    public override MonkeyKingPrototype Clone()
+    {
+        // 调用MemberwiseClone方法实现的是浅拷贝 浅拷贝是指当对象的字段值被拷贝时，字段引用的对象不会被拷贝。
+        return (MonkeyKingPrototype)this.MemberwiseClone();
+    }
+}
---
+++ _06_PrototypePattern/Prototype/MonkeyKingPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_PrototypePattern.Prototype;
+
+public abstract class MonkeyKingPrototype
+{
+    public string Id { get; set; }
+
+    protected MonkeyKingPrototype(string id)
+    {
+        Id = id;
+    }
+
+    public abstract MonkeyKingPrototype Clone();
+}
